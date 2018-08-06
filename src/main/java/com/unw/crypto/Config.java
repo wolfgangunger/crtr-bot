a28077d791fc1c9ba68ed437268c5ee78dc91c84
Wolfgang Unger <wolfgang.unger@gmx.de>
2018-08-06 18:31:32 +0200

    csv files

--- src/main/java/com/unw/crypto/Config.java
+++ src/main/java/com/unw/crypto/Config.java
@@ -13,13 +13,14 @@ public final class Config {
     
     public static final int HEIGHT = 680;
     public static final int WIDTH = 1288;
-    public static final String csv_file = "bitfinexUSD_2016_from_Nov.csv";
+   // public static final String csv_file = "bitfinexUSD_2016_from_Nov.csv";
+    public static final String csv_file = "bitstampEUR_2018.csv";    
     //public static final String csv_file = "bitfinexUSD-from_2017.csv";    
     //public static final String csv_file = "bitstamp_trades_from_20131125_usd.csv";
     
-    public static final String fromDate = "2016-11-15";
-    public static final String untilDate = "2016-12-31";   
-    //public static final String fromDate = "2018-01-01";
-    //public static final String untilDate = "2018-04-30";   
+    //public static final String fromDate = "2016-11-15";
+    //public static final String untilDate = "2016-12-31";   
+    public static final String fromDate = "2018-04-30";
+    public static final String untilDate = "2018-07-15";   
     
 }
