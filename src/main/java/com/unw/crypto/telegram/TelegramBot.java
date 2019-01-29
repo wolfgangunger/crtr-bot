cf677414cd8af5da4a6bfd7cc8b8b978fcc483f6
wolfgangunger <wolfgang.unger@gmx.de>
2019-01-29 16:10:10 +0100

    jaaf

--- src/main/java/com/unw/crypto/telegram/TelegramBot.java
+++ src/main/java/com/unw/crypto/telegram/TelegramBot.java
@@ -16,6 +16,9 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
  */
 public class TelegramBot extends TelegramLongPollingBot {
     
+    //https://core.telegram.org/bots#3-how-do-i-create-a-bot
+    // https://github.com/rubenlagus/TelegramBots
+    
 //    Done! Congratulations on your new bot. You will find it at t.me/crtr_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
 //
 //Use this token to access the HTTP API:
